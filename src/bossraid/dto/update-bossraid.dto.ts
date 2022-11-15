2a7ae31ba3530162a751312aaad0bec6a63654b6
Jeong Jae Ha <jaeha2324@gmail.com>
2022-11-15 11:35:05 +0900

    feat: 레이드 상태확인, 입장, 종료 api

--- src/bossraid/dto/update-bossraid.dto.ts
+++ src/bossraid/dto/update-bossraid.dto.ts
@@ -1,4 +1,17 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsNumber, matches, Matches } from 'class-validator';
 import { CreateBossraidDto } from './create-bossraid.dto';
 
-export class UpdateBossraidDto extends PartialType(CreateBossraidDto) {}
+export class UpdateBossraidDto {
+    @IsNumber()
+    @Matches(/[0-9]/, {
+        message: 'userId(숫자)를 입력해주세요.'
+    })
+    userId: string;
+
+    @IsNumber()
+    @Matches(/[0-9]/, {
+        message: 'recordId(숫자)를 입력해주세요.'
+    })
+    recordId: string;
+}

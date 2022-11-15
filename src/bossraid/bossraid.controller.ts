0e18d27f89be83461e9cf4420132c0d53cb1b69b
Jeong Jae Ha <jaeha2324@gmail.com>
2022-11-15 15:44:28 +0900

    fix: raid end api 로직 수정

--- src/bossraid/bossraid.controller.ts
+++ src/bossraid/bossraid.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Inject, CACHE_MANAGER, BadRequestException, CacheTTL, UseInterceptors, CacheInterceptor, ValidationPipe } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch,  BadRequestException, ValidationPipe } from '@nestjs/common';
 import { BossraidService } from './bossraid.service';
 import { CreateBossraidDto } from './dto/create-bossraid.dto';
 import { UpdateBossraidDto } from './dto/update-bossraid.dto';
 import { HttpService } from '@nestjs/axios'
-import { firstValueFrom, map, Observable } from 'rxjs';
+import { firstValueFrom, map } from 'rxjs';
 import Redis from 'ioredis'
 import { InjectRedis } from '@liaoliaots/nestjs-redis'
 import { BossraidRankDto } from './dto/get-bossraidRank.dto';
+import { config } from 'dotenv'
+
+config();
 
 @Controller('bossraid')
 export class BossraidController {
@@ -16,21 +19,26 @@ export class BossraidController {
     ) {}
     
   @Post('/enter')
-  async raidEnter(@Body(ValidationPipe) createBossraidDto: CreateBossraidDto): Promise<{}> {
-    const { level } = createBossraidDto
-    await firstValueFrom(this.httpService.get('https://dmpilf5svl7rv.cloudfront.net/assignment/backend/bossRaidData.json').pipe(
-      map((res) => {
-        try{
-          let reqLevel = res.data.bossRaids[0].levels[level].level
-          let reqScore = res.data.bossRaids[0].levels[level].score
-          this.redis.set('score', reqScore)
-          this.redis.set('level', reqLevel)
-        } catch (err) {
-          throw new BadRequestException('level을 확인해주세요')
-        }
-      })
-    ));
-    return this.bossraidService.raidEnter(createBossraidDto);
+  async raidEnter(@Body(ValidationPipe) createBossraidDto: CreateBossraidDto) {
+    const { level, userId } = createBossraidDto
+    const maxLevel = Number(process.env.MAX_LEVEL)
+    const getRedisLevel = await this.redis.get(`level${level}`)
+    await this.redis.set(`user${userId}EnterLevel`, level)
+    if(!getRedisLevel && level <= maxLevel) {
+      await firstValueFrom(this.httpService.get('https://dmpilf5svl7rv.cloudfront.net/assignment/backend/bossRaidData.json').pipe(
+        map((res) => {
+          for(let i=0; i<res.data.bossRaids[0].levels.length; i++) {
+            let reqLevel = res.data.bossRaids[0].levels[i].level
+            let reqScore = res.data.bossRaids[0].levels[i].score
+            this.redis.set(`level${i} score`, reqScore)
+            this.redis.set(`level${i}`, reqLevel)
+          }
+        })
+      ));
+    } else if (!getRedisLevel){
+      throw new BadRequestException('check enter level')
+    }
+    return this.bossraidService.raidEnter(createBossraidDto, Number(getRedisLevel));
   }
 
   @Get()
@@ -40,8 +48,14 @@ export class BossraidController {
 
   @Patch('/end')
   async raidEnd(@Body(ValidationPipe) updateBossraidDto: UpdateBossraidDto):Promise<{}> {
-    const redisScore = await this.redis.get('score');
     const { userId, recordId } = updateBossraidDto
+    const redisLevel = await this.redis.get(`user${userId}EnterLevel`);
+    if(!redisLevel) {
+      await this.bossraidService.updateEndTime(userId, recordId)
+      throw new BadRequestException('this raid already end')
+    }
+    const redisScore = await this.redis.get(`level${redisLevel} score`);
+    await this.redis.del(`user${userId}EnterLevel`);
     return await this.bossraidService.raidEnd(userId, recordId, redisScore);
     
   }
